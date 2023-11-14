67385a7e8666a7c9e124dca53313d445a715c551
Eduardo Olivares <28717476+eduardo-olivares1@users.noreply.github.com>
2023-11-13 21:17:37 -0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CMP SCI 192</title>
+    <title>CMP SCI 192 | Home</title>
 </head>
 <body>
     <?php 
